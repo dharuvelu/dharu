2caa6b7d9c0e0173457023dd9ab431b5cb7dbc25
dharuvelu <40588588+dharuvelu@users.noreply.github.com>
2018-06-26 10:23:45 +0530

    odd.java

---
+++ odd.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class odd
+{
+public static void main(String[] arg
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if((a>=1)&&(a<=100000))
+{
+if(a%2==0)
+{
+System.out.print("even");
+}
+else
+{
+System.out.print("odd");
+}
+}
+}}
