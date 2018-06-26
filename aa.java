7e4e3beb137976016408389c2bfd13e9efffa516
dharuvelu <40588588+dharuvelu@users.noreply.github.com>
2018-06-26 10:12:07 +0530

    aa.java

--- aa.java
+++ aa.java
@@ -1,1 +1,15 @@
-# dharu
+import java.io.*;
+import java.util.*;
+class aa
+{
+  public static void main(String[] arg)
+  {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if((a>=1)&&(a<=100000))
+    {
+      System.out.print("positive");
+    }
+  }
+}
+                           
