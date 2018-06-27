9f3095c7319637bff4c0c4ea4b8fc9731547ca58
dharuvelu <40588588+dharuvelu@users.noreply.github.com>
2018-06-27 18:25:08 +0530

    Sum.java

---
+++ Sum.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class Sum
+{
+public static void main(String[] arg)
+{
+Scanner sc=new Scanner(Sysyem.in);
+int n=sc.nextInt();
+int a=sc.nextInt();
+int sum=0;
+int b[]=new int[n];
+for(int i=0;i<ni++)
+{
+b[i]=sc.nextInt();
+}
+for(int i=0;i<a;i++)
+{
+sum=sum+;
+}
+System.out.print(sum);
+}
+}
