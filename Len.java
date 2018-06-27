b61958337827472fe61b5694051f8a3dc99f1bfb
dharuvelu <40588588+dharuvelu@users.noreply.github.com>
2018-06-27 18:40:52 +0530

    Len.java

---
+++ Len.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class Len
+{
+public static void main(String[] arg)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+System.out.print(a.length);
+}
+}
